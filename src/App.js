1832de2e27b6f7a33ed4880d78e4b9a14e621d00
Makoto Ogata <ogatamakoto1976@gmail.com>
2020-11-11 23:46:45 +0900

    make field

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Navigation from "./components/Navigation";
 import Field from "./components/Field";
 import Button from "./components/Button";
