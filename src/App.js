b556a799419f2381e22dea9dc0e094e1f7de995f
Makoto Ogata <ogatamakoto1976@gmail.com>
2020-11-13 01:34:20 +0900

    Implemented a snake movement.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import Navigation from "./components/Navigation";
 import Field from "./components/Field";
 import Button from "./components/Button";
 import ManipulationPanel from "./components/ManipulationPanel";
+import { initFields } from "./utils";
+
+const initialPosition = { x: 17, y: 17}
+const initialValues = initFields(35, initialPosition);
+const defaultInterval = 100;
+
+let timer = undefined;
+
+const unsubscribe = () =>{
+  if(!timer) {
+    return
+  }
+  clearInterval(timer);
+}
 
 function App() {
+
+  const [fields, setFields] = useState(initialValues);
+  const [position, setPosition] = useState();
+  const [tick, setTick] = useState(0);
+
+  useEffect(() => {
+    setPosition(initialPosition);
+    timer = setInterval(() => {
+      setTick(tick => tick + 1);
+    }, defaultInterval)
+    return unsubscribe
+  }, []);
+
+  useEffect(() => {
+    if(!position) {
+      return ;
+    }
+    goUp();
+  }, [tick]);
+
+  const goUp = () =>{
+    const {x, y} = position;
+    const nextY = Math.max(y -1, 0);
+    fields[y][x] = '';
+    fields[nextY][x] = 'snake';
+    setPosition({x, y: nextY});
+    setFields(fields);
+  }
+
   return (
     <div className="App">
       <header className="header">
@@ -14,7 +57,7 @@ function App() {
         <Navigation />
       </header>
       <main className="main">
-        <Field />
+        <Field fields={fields} />
       </main>
       <footer className="footer">
         <Button />
