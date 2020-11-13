a2640575ef3101d50f4d52fd557133fb95e09a80
Makoto Ogata <ogatamakoto1976@gmail.com>
2020-11-13 23:49:43 +0900

    added a gane over function

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ const initialPosition = { x: 17, y: 17}
 const initialValues = initFields(35, initialPosition);
 const defaultInterval = 100;
 
+const GameStatus = Object.freeze({
+  init: 'init',
+  playing: 'playing',
+  suspended: 'suspended',
+  gameover: 'gameover',
+});
+
 let timer = undefined;
 
 const unsubscribe = () =>{
@@ -18,10 +25,24 @@ const unsubscribe = () =>{
   clearInterval(timer);
 }
 
+const isCollision = (filedSize, position) => {
+  if(position.y < 0 || position.x < 0) {
+    return true;
+  }
+
+  if (position.y > filedSize - 1 || position.x > filedSize - 1) {
+    return true;
+  }
+
+  return false;
+};
+
+
 function App() {
 
   const [fields, setFields] = useState(initialValues);
   const [position, setPosition] = useState();
+  const [status, setStatus] = useState(GameStatus.init);
   const [tick, setTick] = useState(0);
 
   useEffect(() => {
@@ -33,19 +54,38 @@ function App() {
   }, []);
 
   useEffect(() => {
-    if(!position) {
+    if(!position || status !== GameStatus.playing) {
       return ;
     }
-    goUp();
+    const canContinue = goUp();
+    if(!canContinue) {
+      setStatus(GameStatus.gameover)
+    }
   }, [tick]);
+
+  const onStart = () => setStatus(GameStatus.playing);
+
+  const onRestart = () => {
+    timer = setInterval(() => {
+      setTick((tick => tick + 1))
+    }, defaultInterval)
+    setStatus(GameStatus.init)
+    setPosition(initialPosition)
+    setFields(initFields(35, initialPosition))
+  }
 
   const goUp = () =>{
     const {x, y} = position;
-    const nextY = Math.max(y -1, 0);
+    const newPosition = {x, y: y -1}
+    if(isCollision(fields.length, newPosition)){
+      unsubscribe()
+      return false
+    }
     fields[y][x] = '';
-    fields[nextY][x] = 'snake';
-    setPosition({x, y: nextY});
+    fields[newPosition.y][x] = 'snake';
+    setPosition(newPosition);
     setFields(fields);
+    return true;
   }
 
   return (
@@ -60,7 +100,7 @@ function App() {
         <Field fields={fields} />
       </main>
       <footer className="footer">
-        <Button />
+        <Button status={status} onStart={onStart} onRestart={onRestart} />
         <ManipulationPanel />
       </footer>
 
